8d5db56498223cd4185438617179f9ddbf7dc579
Kaliatech <josh@jgstechnical.com>
2022-02-15 14:46:04 -0500

    Enable HTTPS vite dev server

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3443,
+    https: true,
+    // Uncomment to allow access from network
+    // (or use `npm run dev -- -- host=0.0.0.0`)
+    //host: '0.0.0.0',
+  },
+})
