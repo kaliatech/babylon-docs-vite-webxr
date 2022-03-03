4156023d50c932768936a3adb567e580bb1c6395
Josh Sanderson <josh@jgstechnical.com>
2022-03-03 13:28:21 -0500

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3443,
-    https: true,
+    https: false,
     // Uncomment to allow access from network
     // (or use `npm run dev -- -- host=0.0.0.0`)
-    //host: '0.0.0.0',
-  },
-})
+    host: "0.0.0.0"
+  }
+});
